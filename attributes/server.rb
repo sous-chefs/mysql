4ba72a1e8e2fe5ad810f26c3121c04cf2fcac972
Mark Luntzel <mark@luntzel.com>
2012-06-28 14:00:35 -0700

    making port an attribute

--- attributes/server.rb
+++ attributes/server.rb
@@ -18,6 +18,7 @@
 #
 
 default['mysql']['bind_address']               = attribute?('cloud') ? cloud['local_ipv4'] : ipaddress
+default['mysql']['port']                       = 3306
 
 case node["platform"]
 when "centos", "redhat", "fedora", "suse", "scientific", "amazon"
