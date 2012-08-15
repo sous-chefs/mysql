00fb2f7699af22c714def411feaacdb678b17bc8
jhowarth <jhowarth@riotgames.com>
2012-08-15 14:11:43 -0700

    Make sure mysql chef_gem is ran at runtime.

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -1,3 +1,12 @@
 node.set['build_essential']['compiletime'] = true
 include_recipe "build-essential"
-chef_gem "mysql"
+
+mysql_gem = chef_gem "mysql" do
+  action :nothing
+end
+
+ruby_block "install the mysql chef_gem at run time" do
+  block do
+    mysql_gem.run_action(:install)
+  end
+end
