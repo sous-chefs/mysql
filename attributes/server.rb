ed922c13c380cb73c7e4c7885761a0af21241025
Josiah Kiehl <josiah@skirmisher.net>
2012-09-17 14:54:12 -0700

    Add config options to my.cnf template

--- attributes/server.rb
+++ attributes/server.rb
@@ -19,11 +19,19 @@
 
 default['mysql']['bind_address']               = attribute?('cloud') ? cloud['local_ipv4'] : ipaddress
 default['mysql']['port']                       = 3306
+default['mysql']['nice']                       = 0
 
 case node["platform"]
 when "centos", "redhat", "fedora", "suse", "scientific", "amazon"
-  default['mysql']['server']['packages']      = %w{mysql-server}
-  default['mysql']['service_name']            = "mysqld"
+  if node["mysql"]["version"].to_f >= 5.5
+    default['mysql']['server']['packages']      = %w{MySQL-server}
+    default['mysql']['service_name']            = "mysql"
+    set['mysql']['pid_file']                    = "/var/run/mysql/mysql.pid"
+  else
+    default['mysql']['server']['packages']      = %w{mysql-server}
+    default['mysql']['service_name']            = "mysqld"
+    set['mysql']['pid_file']                    = "/var/run/mysqld/mysqld.pid"
+  end
   default['mysql']['basedir']                 = "/usr"
   default['mysql']['data_dir']                = "/var/lib/mysql"
   default['mysql']['root_group']              = "root"
@@ -33,7 +41,6 @@ when "centos", "redhat", "fedora", "suse", "scientific", "amazon"
   set['mysql']['conf_dir']                    = '/etc'
   set['mysql']['confd_dir']                   = '/etc/mysql/conf.d'
   set['mysql']['socket']                      = "/var/lib/mysql/mysql.sock"
-  set['mysql']['pid_file']                    = "/var/run/mysqld/mysqld.pid"
   set['mysql']['old_passwords']               = 1
   set['mysql']['grants_path']                 = "/etc/mysql_grants.sql"
   # RHEL/CentOS mysql package does not support this option.
@@ -108,11 +115,23 @@ default['mysql']['auto-increment-increment']        = 1
 default['mysql']['auto-increment-offset']           = 1
 
 default['mysql']['allow_remote_root']               = false
+default['mysql']['tunable']['character-set-server'] = "utf8"
+default['mysql']['tunable']['collation-server']     = "utf8_general_ci"
+default['mysql']['tunable']['event_scheduler']      = 0
 default['mysql']['tunable']['back_log']             = "128"
 default['mysql']['tunable']['key_buffer']           = "256M"
+default['mysql']['tunable']['myisam_sort_buffer_size']   = "8M"
+default['mysql']['tunable']['myisam_max_sort_file_size'] = "2147483648"
+default['mysql']['tunable']['myisam_repair_threads']     = "1"
+default['mysql']['tunable']['myisam_recover']            = "BACKUP"
 default['mysql']['tunable']['max_allowed_packet']   = "16M"
 default['mysql']['tunable']['max_connections']      = "800"
-default['mysql']['tunable']['max_heap_table_size']  = "32M"
+default['mysql']['tunable']['max_connect_errors']   = "10"
+default['mysql']['tunable']['concurrent_insert']    = "2"
+default['mysql']['tunable']['connect_timeout']      = "10"
+default['mysql']['tunable']['tmp_table_size']       = "32M"
+default['mysql']['tunable']['max_heap_table_size']  = node['mysql']['tunable']['tmp_table_size']
+default['mysql']['tunable']['bulk_insert_buffer_size'] = node['mysql']['tunable']['tmp_table_size']
 default['mysql']['tunable']['myisam_recover']       = "BACKUP"
 default['mysql']['tunable']['net_read_timeout']     = "30"
 default['mysql']['tunable']['net_write_timeout']    = "30"
@@ -122,26 +141,52 @@ default['mysql']['tunable']['thread_cache']         = "128"
 default['mysql']['tunable']['thread_cache_size']    = 8
 default['mysql']['tunable']['thread_concurrency']   = 10
 default['mysql']['tunable']['thread_stack']         = "256K"
+default['mysql']['tunable']['sort_buffer_size']     = "2M"
+default['mysql']['tunable']['read_buffer_size']     = "128k"
+default['mysql']['tunable']['read_rnd_buffer_size'] = "256k"
+default['mysql']['tunable']['join_buffer_size']     = "128k"
 default['mysql']['tunable']['wait_timeout']         = "180"
+default['mysql']['tunable']['open-files-limit']     = "8192"
+default['mysql']['tunable']['open-files']           = "1024"
+
+default['mysql']['tunable']['sql_mode'] = nil
+
+default['mysql']['tunable']['skip-character-set-client-handshake'] = false
+default['mysql']['tunable']['skip-name-resolve']                   = false
 
 default['mysql']['tunable']['log_bin']                         = nil
 default['mysql']['tunable']['log_bin_trust_function_creators'] = false
-default['mysql']['tunable']['relay_log']                       =  nil
+default['mysql']['tunable']['relay_log']                       = nil
+default['mysql']['tunable']['relay_log_index']                 = nil
 default['mysql']['tunable']['log_slave_updates']               = false
+default['mysql']['tunable']['binlog_format']                   = "MIXED"
 default['mysql']['tunable']['sync_binlog']                     = 0
 default['mysql']['tunable']['skip_slave_start']                = false
 
 default['mysql']['tunable']['log_error']                       = nil
+default['mysql']['tunable']['log_warnings']                    = false
 default['mysql']['tunable']['log_queries_not_using_index']     = true
 default['mysql']['tunable']['log_bin_trust_function_creators'] = false
 
-default['mysql']['tunable']['innodb_buffer_pool_size']         = "128M"
 default['mysql']['tunable']['innodb_log_file_size']            = "5M"
+default['mysql']['tunable']['innodb_buffer_pool_size']         = "128M"
+default['mysql']['tunable']['innodb_buffer_pool_instances']    = "4"
 default['mysql']['tunable']['innodb_additional_mem_pool_size'] = "8M"
 default['mysql']['tunable']['innodb_data_file_path']           = "ibdata1:10M:autoextend"
 default['mysql']['tunable']['innodb_flush_log_at_trx_commit']  = "1"
 default['mysql']['tunable']['innodb_flush_method']             = false
 default['mysql']['tunable']['innodb_log_buffer_size']          = "8M"
+default['mysql']['tunable']['innodb_write_io_threads']         = "4"
+default['mysql']['tunable']['innodb_io_capacity']              = "200"
+default['mysql']['tunable']['innodb_read_io_threads']          = "4"
+default['mysql']['tunable']['innodb_file_per_table']           = true
+default['mysql']['tunable']['innodb_lock_wait_timeout']        = "60"
+default['mysql']['tunable']['innodb_thread_concurrency']       = "8" #recommend 2x core quantity
+default['mysql']['tunable']['innodb_commit_concurrency']       = "0" #recommend 4x num disks
+default['mysql']['tunable']['innodb_support_xa']               = true
+default['mysql']['tunable']['skip-innodb-doublewrite']         = false
+
+default['mysql']['tunable']['transaction-isolation'] = nil
 
 default['mysql']['tunable']['query_cache_limit']    = "1M"
 default['mysql']['tunable']['query_cache_size']     = "16M"
@@ -151,3 +196,11 @@ default['mysql']['tunable']['long_query_time']      = 2
 
 default['mysql']['tunable']['expire_logs_days']     = 10
 default['mysql']['tunable']['max_binlog_size']      = "100M"
+default['mysql']['tunable']['binlog_cache_size']    = "32K"
+
+default['mysql']['tmpdir'] = "/tmp"
+default['mysql']['read_only'] = false
+
+default['mysql']['log_dir'] = node['mysql']['log_dir']
+default['mysql']['log_files_in_group'] = false
+default['mysql']['innodb_status_file'] = false
