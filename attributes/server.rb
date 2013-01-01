0d91bb994a982916e54a07225d90f2b4c3dec93a
John Dyer <jdyer@voxeolabs.com>
2012-12-31 23:13:50 -0500

    COOK-2145 - Added two new attributes to delete passwordless and anonymous accounts

--- attributes/server.rb
+++ attributes/server.rb
@@ -169,3 +169,7 @@ default['mysql']['tunable']['long_query_time']      = 2
 
 default['mysql']['tunable']['expire_logs_days']     = 10
 default['mysql']['tunable']['max_binlog_size']      = "100M"
+
+default['mysql']['delete_anonymous_users']          =  true
+default['mysql']['delete_passwordless_users']       =  true
+
