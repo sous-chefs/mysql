02dc9d347aa7486404ecc469ccd640d5a23d7fa9
ctdk <jbingham@gmail.com>
2013-02-05 11:43:34 -0800

    Add percona yum repo

--- recipes/percona_repo.rb
+++ recipes/percona_repo.rb
@@ -40,4 +40,20 @@ case node['platform']
       key node['mysql']['percona']['key_id']
       action :add
     end
+  when "centos", "amazon", "redhat"
+    include_recipe "yum"
+    yum_key "RPM-GPG-KEY-percona" do
+      url "http://www.percona.com/downloads/RPM-GPG-KEY-percona"
+      action :add
+    end
+    arch = node['kernel']['machine']
+    arch = "i386" unless arch == "x86_64"
+    pversion = node['platform_version']
+    yum_repository "percona"
+      repo_name "Percona"
+      description "Percona Repo"
+      url "http://repo.percona.com/centos/#{pversion}/os/#{arch}/"
+      key "RPM-GPG-KEY-percona"
+      action :add 
+    end
 end
