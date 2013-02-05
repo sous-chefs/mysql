9f3ee891b6154651baec15b64b78e792119d69ee
ctdk <jbingham@gmail.com>
2013-02-05 10:00:36 -0800

    Add recipe for adding the percona repo if needed

--- recipes/percona_repo.rb
+++ recipes/percona_repo.rb
@@ -19,30 +19,30 @@
 
 
 case node['platform']
-  when "ubuntu", "debian"
-    include_recipe "apt"
-    apt_repository "percona" do
-      uri "http://repo.percona.com/apt"
-      distribution node['lsb']['codename']
-      components [ "main" ]
-      keyserver "kyes.gnupg.net"
-      key node['mysql']['percona']['key_id']
-      action :add
-    end
-  when "centos", "amazon", "redhat"
-    include_recipe "yum"
-    yum_key "RPM-GPG-KEY-percona" do
-      url "http://www.percona.com/downloads/RPM-GPG-KEY-percona"
-      action :add
-    end
-    arch = node['kernel']['machine']
-    arch = "i386" unless arch == "x86_64"
-    pversion = node['platform_version']
-    yum_repository "percona" do
-      repo_name "Percona"
-      description "Percona Repo"
-      url "http://repo.percona.com/centos/#{pversion}/os/#{arch}/"
-      key "RPM-GPG-KEY-percona"
-      action :add 
-    end
+when "ubuntu", "debian"
+  include_recipe "apt"
+  apt_repository "percona" do
+    uri "http://repo.percona.com/apt"
+    distribution node['lsb']['codename']
+    components [ "main" ]
+    keyserver "kyes.gnupg.net"
+    key node['mysql']['percona']['key_id']
+    action :add
+  end
+when "centos", "amazon", "redhat"
+  include_recipe "yum"
+  yum_key "RPM-GPG-KEY-percona" do
+    url "http://www.percona.com/downloads/RPM-GPG-KEY-percona"
+    action :add
+  end
+  arch = node['kernel']['machine']
+  arch = "i386" unless arch == "x86_64"
+  pversion = node['platform_version']
+  yum_repository "percona" do
+    repo_name "Percona"
+    description "Percona Repo"
+    url "http://repo.percona.com/centos/#{pversion}/os/#{arch}/"
+    key "RPM-GPG-KEY-percona"
+    action :add
+  end
 end
