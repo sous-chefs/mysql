b4f9e77bcb72b7fd97b4eff0a8f5485063f97a02
ctdk <jbingham@gmail.com>
2013-02-05 12:39:14 -0800

    Apt cookbook behavior changed slightly somewhere along the line; cleaned up a bit to be cleaner and smoother

--- recipes/percona_repo.rb
+++ recipes/percona_repo.rb
@@ -21,22 +21,11 @@
 case node['platform']
   when "ubuntu", "debian"
     include_recipe "apt"
-    execute "add percona key to gpg" do
-      command "gpg --keyserver keys.gnupg.net --homedir /root " +
-	"--recv-keys #{node['mysql']['percona']['key_id']}"
-      not_if "gpg --list-keys #{node['mysql']['percona']['key_id']}"
-    end
-
-    execute "add percona key to apt" do
-      command "gpg --homedir /root --armor " +
-	"--export #{node['mysql']['percona']['key_id']} | apt-key add -"
-      not_if "apt-get key list #{node['mysql']['percona']['key_id']}"
-    end
-
     apt_repository "percona" do
       uri "http://repo.percona.com/apt"
       distribution node['lsb']['codename']
       components [ "main" ]
+      keyserver "kyes.gnupg.net"
       key node['mysql']['percona']['key_id']
       action :add
     end
