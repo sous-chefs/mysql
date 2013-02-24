54c7b032a7b1a15cb3990972b4ffa42ca0135c99
Antono Vasiljev <antono.vasiljev@gmail.com>
2013-02-24 17:51:53 +0700

    COOK-2158: do not run apt-get update on each run
    Should fix: http://tickets.opscode.com/browse/COOK-2158

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -23,6 +23,11 @@
 execute "apt-get update" do
   ignore_failure true
   action :nothing
+  # tip: to suppress this running every time, just use the apt cookbook
+  not_if do
+    ::File.exists?('/var/lib/apt/periodic/update-success-stamp') &&
+    ::File.mtime('/var/lib/apt/periodic/update-success-stamp') > Time.now - 86400*2
+  end
 end.run_action(:run) if node['platform_family'] == "debian"
 
 node.set['build_essential']['compiletime'] = true
