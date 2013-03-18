9c3234acd002a5cc9e83926941953023c27c60c5
rackerjoe <breu@breu.org>
2013-03-18 09:03:34 -0500

    allow root to connect from a remote network

--- attributes/server.rb
+++ attributes/server.rb
@@ -143,6 +143,7 @@ default['mysql']['auto-increment-increment']        = 1
 default['mysql']['auto-increment-offset']           = 1
 
 default['mysql']['allow_remote_root']               = false
+default['mysql']['root_network_acl']                = nil
 default['mysql']['tunable']['character-set-server'] = "utf8"
 default['mysql']['tunable']['collation-server']     = "utf8_general_ci"
 default['mysql']['tunable']['back_log']             = "128"
