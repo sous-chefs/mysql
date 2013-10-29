aa75f0afffced1ec33fabbe30a66691d7dd85d7c
Christoph Hartmann <chris@lollyrock.com>
2013-10-28 21:26:03 -0400

    [COOK-3697] MySQL Security Hardening
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- attributes/server.rb
+++ attributes/server.rb
@@ -260,3 +260,20 @@ unless node['platform_family'] == 'rhel' && node['platform_version'].to_i < 6
   default['mysql']['tunable']['event_scheduler']  = 0
   default['mysql']['tunable']['binlog_format']    = 'statement' if node['mysql']['tunable']['log_bin']
 end
+
+# security options
+# @see http://www.symantec.com/connect/articles/securing-mysql-step-step
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-options.html#option_mysqld_chroot
+default['mysql']['security']['chroot']                  = nil
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-options.html#option_mysqld_safe-user-create
+default['mysql']['security']['safe_user_create']        = nil
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-options.html#option_mysqld_secure-auth
+default['mysql']['security']['secure_auth']             = nil
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-options.html#option_mysqld_symbolic-links
+default['mysql']['security']['skip_symbolic_links']     = nil
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-options.html#option_mysqld_secure-file-priv
+default['mysql']['security']['secure_file_priv']        = nil
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-options.html#option_mysqld_skip-show-database
+default['mysql']['security']['skip_show_database']      = nil
+# @see http://dev.mysql.com/doc/refman/5.7/en/server-system-variables.html#sysvar_local_infile
+default['mysql']['security']['local_infile']            = nil
