sous-chefs/mysql

commit 839273e5bf073efaeb0330939fd597572df78dc6

[COOK-3773] Fixing immediate restarts on template
Signed-off-by: Sean OMeara <someara@opscode.com>

recipes/server.rb
@@ -149,7 +149,8 @@ unless platform_family?('mac_os_x')
                      false
                    end
 
-  template "#{node['mysql']['conf_dir']}/my.cnf" do
+  template 'initial-my.cnf' do
+    path "#{node['mysql']['conf_dir']}/my.cnf"
     source 'my.cnf.erb'
     owner 'root' unless platform? 'windows'
     group node['mysql']['root_group'] unless platform?('windows')
@@ -224,7 +225,9 @@ else
     action       :enable
   end
 
-  template "#{node['mysql']['conf_dir']}/my.cnf" do
+unless platform_family?('mac_os_x')
+  template 'final-my.cnf' do
+    path "#{node['mysql']['conf_dir']}/my.cnf"
     source 'my.cnf.erb'
     owner 'root' unless platform? 'windows'
     group node['mysql']['root_group'] unless platform? 'windows'

commit e30c5b942b71f2db5a4119f36813139f9955d1e1

fix regression introduced in 839273e5bf073efaeb0330939fd597572df78dc6

recipes/server.rb
@@ -225,7 +225,6 @@ else
     action       :enable
   end
 
-unless platform_family?('mac_os_x')
   template 'final-my.cnf' do
     path "#{node['mysql']['conf_dir']}/my.cnf"
     source 'my.cnf.erb'