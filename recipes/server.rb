08d8589a0716404f9024fc8c98a741345fa07bed
Moshe Bergman <bergmanm@gmail.com>
2013-11-10 20:46:06 -0800

    remove set_unless statement

--- recipes/server.rb
+++ recipes/server.rb
@@ -33,9 +33,9 @@ if Chef::Config[:solo]
   end
 else
   # generate all passwords
-  node.set_unless['mysql']['server_debian_password'] = secure_password
-  node.set_unless['mysql']['server_root_password']   = secure_password
-  node.set_unless['mysql']['server_repl_password']   = secure_password
+  node['mysql']['server_debian_password'] = secure_password unless node['mysql']['server_debian_password']
+  node['mysql']['server_root_password']   = secure_password unless node['mysql']['server_root_password']
+  node['mysql']['server_repl_password']   = secure_password unless node['mysql']['server_repl_password']
   node.save
 end
 
