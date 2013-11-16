55b50c5b2230d7a6654af4c910f47037ff0caec9
Will Milton <wa.milton@gmail.com>
2013-11-16 16:21:04 -0600

    foodcritic fixes

--- recipes/_server_debian.rb
+++ recipes/_server_debian.rb
@@ -84,10 +84,9 @@ directory node['mysql']['data_dir'] do
 end
 
 # debian package still has traditional init script
-if node.platform_family == 'ubuntu'
-  template '/etc/init/mysql.conf' do
-    source 'init-mysql.conf.erb'
-  end
+template '/etc/init/mysql.conf' do
+  source 'init-mysql.conf.erb'
+  only_if node['platform_family'] == 'ubuntu'
 end
 
 template '/etc/apparmor.d/usr.sbin.mysqld' do
