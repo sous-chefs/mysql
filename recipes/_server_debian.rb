1d9b7a2c1486a685d41ae0d059f09502cb9bfb66
Matthew Thode <mthode@mthode.org>
2014-01-31 15:55:40 -0600

    now with passing stuffs

--- recipes/_server_debian.rb
+++ recipes/_server_debian.rb
@@ -79,18 +79,17 @@ template '/etc/mysql_grants.sql' do
   notifies :run, 'execute[install-grants]', :immediately
 end
 
+cmd = install_grants_cmd
+execute 'install-grants' do
+  command cmd
+  action :nothing
+end
+
 template '/etc/mysql/debian.cnf' do
   source 'debian.cnf.erb'
   owner  'root'
   group  'root'
   mode   '0600'
-end
-
-
-cmd = install_grants_cmd
-execute 'install-grants' do
-  command cmd
-  action :nothing
 end
 
 #----
