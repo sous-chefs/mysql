2a61d756d76a3fa0642de2c136035659091d8029
Zvi "Viz" Effron <viz@flippedperspective.com>
2014-02-23 05:52:32 -0800

    Add fedora support via mysql-community

---
+++ attributes/server_fedora.rb
@@ -0,0 +1,36 @@
+case node['platform_family']
+when 'fedora'
+
+  default['mysql']['version'] = '5.6'
+
+  # Probably driven from wrapper cookbooks, environments, or roles.
+  # Keep in this namespace for backwards compat
+  default['mysql']['data_dir'] = '/var/lib/mysql'
+
+  default['mysql']['server']['packages'] = ['mysql-community-server']
+
+  default['mysql']['server']['slow_query_log']       = 1
+  default['mysql']['server']['slow_query_log_file']  = '/var/log/mysql/slow.log'
+
+  # Platformisms.. filesystem locations and such.
+  default['mysql']['server']['basedir'] = '/usr'
+  default['mysql']['server']['tmpdir'] = ['/tmp']
+
+  default['mysql']['server']['directories']['run_dir']              = '/var/run/mysqld'
+  default['mysql']['server']['directories']['log_dir']              = '/var/lib/mysql'
+  default['mysql']['server']['directories']['slow_log_dir']         = '/var/log/mysql'
+  default['mysql']['server']['directories']['confd_dir']            = '/etc/mysql/conf.d'
+
+  default['mysql']['server']['mysqladmin_bin']       = '/usr/bin/mysqladmin'
+  default['mysql']['server']['mysql_bin']            = '/usr/bin/mysql'
+
+  default['mysql']['server']['pid_file']             = '/var/run/mysqld/mysqld.pid'
+  default['mysql']['server']['socket']               = '/var/lib/mysql/mysql.sock'
+  default['mysql']['server']['grants_path']          = '/etc/mysql_grants.sql'
+  default['mysql']['server']['old_passwords']        = 1
+  default['mysql']['server']['service_name']        = 'mysqld'
+
+  # RHEL/CentOS mysql package does not support this option.
+  default['mysql']['tunable']['innodb_adaptive_flushing'] = false
+  default['mysql']['server']['skip_federated'] = false
+end
