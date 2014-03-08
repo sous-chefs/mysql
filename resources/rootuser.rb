d2cb47cf2fac94edbebcf99f226758ae0c689a56
Sean OMeara <someara@opscode.com>
2014-03-08 00:35:44 -0500

    removing experiemtal cruft

--- resources/rootuser.rb
+++
@@ -1,5 +0,0 @@
-actions :create
-
-attribute :user, :kind_of => [String], :default => 'root'
-attribute :host, :kind_of => [String], :default => 'localhost'
-attribute :password, :kind_of => [String], :default => nil
