aa26711fe35bbe8c49a15372a68bd03eebb3f334
Tom Noonan II <thomas.noonan@rackspace.com>
2014-03-10 01:15:25 -0500

    Bump patch

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'rackspace-cookbooks@rackspace.com'
 license           'Apache 2.0'
 description       'Installs and configures mysql for client or server'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '6.0.1'
+version           '6.0.2'
 
 recipe            'rackspace_mysql', 'Includes the client recipe to configure a client'
 recipe            'rackspace_mysql::client', 'Installs packages required for mysql clients using run_action magic'
