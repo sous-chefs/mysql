42fea1b34dd5dd1dfbaff78e178b4a2542cea009
Sean OMeara <someara@opscode.com>
2014-03-14 00:08:19 -0400

    adding Opscode::Helpers::MysqlPlatformMap and spec

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,57 +1,63 @@
 module Opscode
   module Mysql
     module Helpers
-      def mysql_platforms
-        @mysql_platforms ||= {
-          'rhel' => {
-            '5' => {
-              '5.0' => 'mysql-server',
-              '5.1' => 'mysql51-mysql-server',
-              '5.5' => 'mysql55-mysql-server'
+      class MysqlPackageMap
+        def self.mysql_package_map
+          @mysql_package_map ||= {
+            'rhel' => {
+              '5' => {
+                '5.0' => 'mysql-server',
+                '5.1' => 'mysql51-mysql-server',
+                '5.5' => 'mysql55-mysql-server'
+              },
+              '6' => {
+                '5.1' => 'mysql-server'
+              }
             },
-            '6' => {
-              '5.1' => 'mysql-server'
-            }
-          },
-          'amazon' => {
-            '2013.09' => {
-              '5.1' => 'mysql-server'
-            }
-          },
-          'fedora' => {
-            '19' => {
-              '5.1' => 'community-mysql-server'
-            }
-          },
-          'debian' => {
-            '7' => {
-              '5.5' => 'mysql-server-5.5'
-            }
-          },
-          'ubuntu' => {
-            '10.04' => {
-              '5.1' => 'mysql-server-5.1'
+            'amazon' => {
+              '2013.09' => {
+                '5.1' => 'mysql-server'
+              }
             },
-            '12.04' => {
-              '5.5' => 'mysql-server-5.5'
+            'fedora' => {
+              '19' => {
+                '5.1' => 'community-mysql-server'
+              }
             },
-            '13.10' => {
-              '5.5' => 'mysql-server-5.5'
-            }
-          },
-          'smartos' => {
-            '1303' => {
-              '5.5' => 'mysql-server-5.5',
-              '5.6' => 'mysql-server-5.6'
-            }
-          },
-          'omnios' => {
-            'r151006c' => {
-              '5.5' => 'mysql-55',
-              '5.6' => 'mysql-56'
+            'debian' => {
+              '7' => {
+                '5.5' => 'mysql-server-5.5'
+              }
+            },
+            'ubuntu' => {
+              '10.04' => {
+                '5.1' => 'mysql-server-5.1'
+              },
+              '12.04' => {
+                '5.5' => 'mysql-server-5.5'
+              },
+              '13.10' => {
+                '5.5' => 'mysql-server-5.5'
+              }
+            },
+            'smartos' => {
+              '1303' => {
+                '5.5' => 'mysql-server-5.5',
+                '5.6' => 'mysql-server-5.6'
+              }
+            },
+            'omnios' => {
+              'r151006c' => {
+                '5.5' => 'mysql-55',
+                '5.6' => 'mysql-56'
+              }
             }
           }
-        }
+        end
+
+        def self.package_for(platform, platform_version, mysql_version)
+          MysqlPlatformMap.mysql_platform_map[platform][platform_version][mysql_version]
+        end
       end
     end
   end
--- spec/unit/helpers_spec.rb
+++ spec/unit/helpers_spec.rb
@@ -1,8 +1,264 @@
 require_relative '../../libraries/helpers.rb'
 
-# describe 'hrm' do
-#   include Opscode::Mysql::Helpers
-#   it 'goes lala' do
-#     expect(mysql_platforms).to eq('lala')
-#   end
-# end
+describe 'mysql_platform_map' do
+
+  context 'for rhel-5' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['5']['5.0']
+        ).to eq('mysql-server')
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['5']['5.1']
+        ).to eq('mysql51-mysql-server')
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['5']['5.5']
+        ).to eq('mysql55-mysql-server')
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['5']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for rhel-6' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['6']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['6']['5.1']
+        ).to eq('mysql-server')
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['6']['5.5']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['rhel']['6']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for amazon-2013.09' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['amazon']['2013.09']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['amazon']['2013.09']['5.1']
+        ).to eq('mysql-server')
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['amazon']['2013.09']['5.5']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['amazon']['2013.09']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for fedora-19' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['fedora']['19']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['fedora']['19']['5.1']
+        ).to eq('community-mysql-server')
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['fedora']['19']['5.5']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['fedora']['19']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for debian-7' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['debian']['7']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['debian']['7']['5.1']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['debian']['7']['5.5']
+        ).to eq('mysql-server-5.5')
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['debian']['7']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for ubuntu-10.04' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['10.04']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['10.04']['5.1']
+        ).to eq('mysql-server-5.1')
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['10.04']['5.5']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['10.04']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for ubuntu-12.04' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['12.04']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['12.04']['5.1']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['12.04']['5.5']
+        ).to eq('mysql-server-5.5')
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['12.04']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for ubuntu-13.10' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['13.10']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['13.10']['5.1']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['13.10']['5.5']
+        ).to eq('mysql-server-5.5')
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['ubuntu']['13.10']['5.6']
+        ).to eq(nil)
+    end
+  end
+
+  context 'for smartos-1303' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['smartos']['1303']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['smartos']['1303']['5.1']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['smartos']['1303']['5.5']
+        ).to eq('mysql-server-5.5')
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['smartos']['1303']['5.6']
+        ).to eq('mysql-server-5.6')
+    end
+  end
+
+  context 'for omnios-r151006c' do
+    it 'returns the package for Mysql 5.0' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['omnios']['r151006c']['5.0']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.1' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['omnios']['r151006c']['5.1']
+        ).to eq(nil)
+    end
+
+    it 'returns the package for Mysql 5.5' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['omnios']['r151006c']['5.5']
+        ).to eq('mysql-55')
+    end
+
+    it 'returns the package for Mysql 5.6' do
+      expect(
+        Opscode::Mysql::Helpers::MysqlPackageMap.mysql_package_map['omnios']['r151006c']['5.6']
+        ).to eq('mysql-56')
+    end
+  end
+end
