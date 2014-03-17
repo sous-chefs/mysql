7e0de64c23817504b52482d4c2d304ea38a2d83a
Sean OMeara <someara@opscode.com>
2014-03-17 11:06:35 -0400

    swaping resource definitions

--- libraries/resource_mysql_service.rb
+++ libraries/resource_mysql_service.rb
@@ -12,24 +12,26 @@ class Chef::Resource::MysqlService < Chef::Resource::LWRPBase
   attribute :service_name, :name_attribute => true, :kind_of => String
 
 #  binding.pry
-  attribute :version, :kind_of => String,
-  :default => MysqlPackageMap.lookup_version(
-    node['platform'],
-    node['platform_version'],
-    node['mysql']['version']
-    ),
-  :callbacks => {
-    "is not supported for #{node['platform']}-#{node['platform_version']}" => lambda do |mysql_version|
-      true unless MysqlPackageMap.package_for(node['platform'], node['platform_version'], mysql_version).nil?
-    end
-  }
+  attribute :version, :kind_of => String, :default => '5.5'
+  # attribute :version, :kind_of => String,
+  # :default => MysqlPackageMap.lookup_version(
+  #   node['platform'],
+  #   node['platform_version'],
+  #   node['mysql']['version']
+  #   ),
+  # :callbacks => {
+  #   "is not supported for #{node['platform']}-#{node['platform_version']}" => lambda do |mysql_version|
+  #     true unless MysqlPackageMap.package_for(node['platform'], node['platform_version'], mysql_version).nil?
+  #   end
+  # }
 
-  attribute :package_name, :kind_of => String,
-  :default => MysqlPackageMap.package_for(
-    node['platform'],
-    node['platform_version'],
-    @version
-    )
+  attribute :package_name, :kind_of => String, :default => 'mysql-55'
+  # attribute :package_name, :kind_of => String,
+  # :default => MysqlPackageMap.package_for(
+  #   node['platform'],
+  #   node['platform_version'],
+  #   @version
+  #   )
 
   attribute :data_dir, :kind_of => String, :default => '/var/lib/mysql'
   attribute :port, :kind_of => String, :default => '3306'
