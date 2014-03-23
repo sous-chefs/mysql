f2573cb560a192af496055e3ddc374cf0c1770fb
Sean OMeara <someara@opscode.com>
2014-03-23 19:07:50 -0400

    MILESTONE: rhel-6 working

--- libraries/provider_mysql_service_rhel.rb
+++ libraries/provider_mysql_service_rhel.rb
@@ -8,41 +8,55 @@ class Chef::Provider::MysqlService::Rhel < Chef::Provider::MysqlService
   end
 
   action :create do
-    case new_resource.version
-    when '5.0'
-      base_dir = nil
-      prefix_dir = '/usr'
-      run_dir = '/var/run/mysqld'
-      pid_file = '/var/run/mysql/mysql.pid'
-      socket_file = '/var/lib/mysql/mysql.sock'
-      package_name = 'mysql-server'
-      service_name = 'mysqld'
-    when '5.1'
-      base_dir = '/opt/rh/mysql51/root'
-      prefix_dir = '/opt/rh/mysql51/root/usr'
-      run_dir = '/opt/rh/mysql51/root/var/run/mysqld/'
-      pid_file = '/var/run/mysql/mysql.pid'
-      socket_file = '/var/lib/mysql/mysql.sock'
-      package_name = 'mysql51-mysql-server'
-      service_name = 'mysql51-mysqld'
-    when '5.5'
-      base_dir = '/opt/rh/mysql55/root'
-      prefix_dir = '/opt/rh/mysql55/root/usr'
-      run_dir = '/opt/rh/mysql55/root/var/run/mysqld/'
-      pid_file = '/var/run/mysql/mysql.pid'
-      socket_file = '/var/lib/mysql/mysql.sock'
-      package_name = 'mysql55-mysql-server'
-      service_name = 'mysql55-mysqld'
+    case node['platform_version'].to_i.to_s
+    when '6' || '2013'
+      case new_resource.version
+      when '5.1'
+        base_dir = ''
+        prefix_dir = '/usr'
+        run_dir = '/var/run/mysqld'
+        pid_file = '/var/run/mysql/mysql.pid'
+        socket_file = '/var/run/mysql/mysql.sock'
+        package_name = 'mysql-server'
+        service_name = 'mysqld'
+      end
+    when '5'
+      case new_resource.version
+      when '5.0'
+        base_dir = ''
+        prefix_dir = '/usr'
+        run_dir = '/var/run/mysqld'
+        pid_file = '/var/run/mysql/mysql.pid'
+        socket_file = '/var/run/mysql/mysql.sock'
+        package_name = 'mysql-server'
+        service_name = 'mysqld'
+      when '5.1'
+        base_dir = '/opt/rh/mysql51/root'
+        prefix_dir = '/opt/rh/mysql51/root/usr'
+        run_dir = '/opt/rh/mysql51/root/var/run/mysqld/'
+        pid_file = '/var/run/mysql/mysql.pid'
+        socket_file = '/var/run/mysql/mysql.sock'
+        package_name = 'mysql51-mysql-server'
+        service_name = 'mysql51-mysqld'
+      when '5.5'
+        base_dir = '/opt/rh/mysql55/root'
+        prefix_dir = '/opt/rh/mysql55/root/usr'
+        run_dir = '/opt/rh/mysql55/root/var/run/mysqld/'
+        pid_file = '/var/run/mysql/mysql.pid'
+        socket_file = '/var/run/mysql/mysql.sock'
+        package_name = 'mysql55-mysql-server'
+        service_name = 'mysql55-mysqld'
+      end
     end
 
     converge_by 'rhel pattern' do
-
       package package_name do
         action :install
       end
 
       directory "#{base_dir}/etc/mysql/conf.d/" do
         owner 'mysql'
+        group 'mysql'
         mode '0750'
         recursive true
         action :create
@@ -50,54 +64,23 @@ class Chef::Provider::MysqlService::Rhel < Chef::Provider::MysqlService
 
       directory run_dir do
         owner 'mysql'
+        group 'mysql'
         mode '0755'
-        action :create
         recursive true
+        action :create
       end
 
       directory new_resource.data_dir do
         owner 'mysql'
+        group 'mysql'
         mode '0750'
+        recursive true
         action :create
-      end
-
-      template "#{base_dir}/etc/my.cnf.erb" do
-        source '5.5/my.cnf.erb'
-        owner 'mysql'
-        mode '0600'
-        variables(
-          :base_dir => base_dir,
-          :data_dir => new_resource.data_dir,
-          :pid_file => pid_file,
-          :socket_file => socket_file,
-          :port => new_resource.port
-          )
-        action :create
-        notifies :run, 'bash[move mysql data to datadir]', :immediately
-        notifies :restart, "service[#{service_name}]"
-      end
-
-      bash 'move mysql data to datadir' do
-        user 'root'
-        code <<-EOH
-        service #{service_name} stop \
-        && mv /var/mysql/* #{new_resource.data_dir}
-        EOH
-        action :nothing
-        only_if "[ '/var/lib/mysql' != #{new_resource.data_dir} ]"
-        only_if "[ `stat -c %h #{new_resource.data_dir}` -eq 2 ]"
-        not_if '[ `stat -c %h /var/lib/mysql/` -eq 2 ]'
       end
 
       service service_name do
         supports :restart => true
         action [:start, :enable]
-        notifies :run, 'execute[wait for mysql]', :immediately
-      end
-
-      execute 'wait for mysql' do
-        command "until [ -S #{socket_file} ] ; do sleep 1 ; done"
-        timeout 10
       end
 
       execute 'assign-root-password' do
@@ -131,22 +114,36 @@ class Chef::Provider::MysqlService::Rhel < Chef::Provider::MysqlService
         command cmd
         action :nothing
       end
-    end
-  end
 
-  action :enable do
-    case new_resource.version
-    when '5.0'
-      service_name = 'mysqld'
-    when '5.1'
-      service_name = 'mysql51-mysqld'
-    when '5.5'
-      service_name = 'mysql55-mysqld'
-    end
+      # FIXME: support user supplied templates
+      template "#{base_dir}/etc/my.cnf" do
+        source "#{new_resource.version}/my.cnf.erb"
+        cookbook 'mysql'
+        owner 'mysql'
+        group 'mysql'
+        mode '0600'
+        variables(
+          :base_dir => base_dir,
+          :data_dir => new_resource.data_dir,
+          :pid_file => pid_file,
+          :socket_file => socket_file,
+          :port => new_resource.port
+          )
+        action :create
+        notifies :run, 'bash[move mysql data to datadir]'
+        notifies :restart, "service[#{service_name}]"
+      end
 
-    converge_by 'configure mysql service resource' do
-      service service_name do
-        action [:start, :enable]
+      bash 'move mysql data to datadir' do
+        user 'root'
+        code <<-EOH
+        service #{service_name} stop \
+        && mv /var/lib/mysql/* #{new_resource.data_dir}
+        EOH
+        action :nothing
+        only_if "[ '/var/lib/mysql' != #{new_resource.data_dir} ]"
+        only_if "[ `stat -c %h #{new_resource.data_dir}` -eq 2 ]"
+        not_if '[ `stat -c %h /var/lib/mysql/` -eq 2 ]'
       end
     end
   end
---
+++ spec/unit/mysql_server/rhel/6/default_stepinto_spec.rb
@@ -0,0 +1,117 @@
+require 'spec_helper'
+
+describe 'mysql_test::mysql_service_attribues' do
+  let(:centos_6_4_default_run) do
+    ChefSpec::Runner.new(
+      :step_into => 'mysql_service',
+      :platform => 'centos',
+      :version => '6.4'
+      ) do |node|
+      node.set['mysql']['service_name'] = 'centos_6_4_default'
+    end.converge('mysql_test::mysql_service_attributes')
+  end
+
+  let(:my_cnf_5_5_content_centos_6_4) do
+    '[client]
+port                           = 3306
+
+[mysqld_safe]
+socket                         = /var/run/mysql/mysql.sock
+
+[mysqld]
+user                           = mysql
+pid-file                       = /var/run/mysql/mysql.pid
+socket                         = /var/run/mysql/mysql.sock
+port                           = 3306
+datadir                        = /var/lib/mysql
+
+[mysql]
+'
+  end
+
+  before do
+    stub_command("/usr/bin/mysql -u root -e 'show databases;'").and_return(true)
+  end
+
+  context 'when using default parameters' do
+    it 'creates mysql_service[centos_6_4_default]' do
+      expect(centos_6_4_default_run).to create_mysql_service('centos_6_4_default').with(
+        :version => '5.1',
+        :port => '3306',
+        :data_dir => '/var/lib/mysql'
+        )
+    end
+
+    it 'steps into mysql_service and installs package[community-mysql-server]' do
+      expect(centos_6_4_default_run).to install_package('mysql-server')
+    end
+
+    it 'steps into mysql_service and creates directory[/etc/mysql/conf.d/]' do
+      expect(centos_6_4_default_run).to create_directory('/etc/mysql/conf.d/').with(
+        :owner => 'mysql',
+        :group => 'mysql',
+        :mode => '0750',
+        :recursive => true
+        )
+    end
+
+    it 'steps into mysql_service and creates directory[/var/run/mysqld]' do
+      expect(centos_6_4_default_run).to create_directory('/var/run/mysqld').with(
+        :owner => 'mysql',
+        :group => 'mysql',
+        :mode => '0755',
+        :recursive => true
+        )
+    end
+
+    it 'steps into mysql_service and creates directory[/var/lib/mysql]' do
+      expect(centos_6_4_default_run).to create_directory('/var/lib/mysql').with(
+        :owner => 'mysql',
+        :group => 'mysql',
+        :mode => '0750',
+        :recursive => true
+        )
+    end
+
+    it 'steps into mysql_service and creates template[/etc/my.cnf]' do
+      expect(centos_6_4_default_run).to create_template('/etc/my.cnf').with(
+        :owner => 'mysql',
+        :group => 'mysql',
+        :mode => '0600'
+        )
+    end
+
+    it 'steps into mysql_service and renders file[/etc/my.cnf]' do
+      expect(centos_6_4_default_run).to render_file('/etc/my.cnf').with_content(my_cnf_5_5_content_centos_6_4)
+    end
+
+    it 'steps into mysql_service and creates bash[move mysql data to datadir]' do
+      expect(centos_6_4_default_run).to_not run_bash('move mysql data to datadir')
+    end
+
+    it 'steps into mysql_service and creates service[mysqld]' do
+      expect(centos_6_4_default_run).to start_service('mysqld')
+      expect(centos_6_4_default_run).to enable_service('mysqld')
+    end
+
+    it 'steps into mysql_service and creates execute[assign-root-password]' do
+      expect(centos_6_4_default_run).to run_execute('assign-root-password').with(
+        :command => '/usr/bin/mysqladmin -u root password ilikerandompasswords'
+        )
+    end
+
+    it 'steps into mysql_service and creates template[/etc/mysql_grants.sql]' do
+      expect(centos_6_4_default_run).to create_template('/etc/mysql_grants.sql').with(
+        :owner => 'root',
+        :group => 'root',
+        :mode => '0600'
+        )
+    end
+
+    it 'steps into mysql_service and creates execute[install-grants]' do
+      expect(centos_6_4_default_run).to_not run_execute('install-grants').with(
+        :command => '/usr/bin/mysql -u root -pilikerandompasswords < /etc/mysql_grants.sql'
+        )
+    end
+  end
+end
