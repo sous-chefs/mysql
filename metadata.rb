d0fe0ee491cb3eabb845205032a42c5d2aa7b787
Sean OMeara <someara@opscode.com>
2014-04-08 14:20:36 -0400

    bumping metadata for devodd

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@getchef.com'
 license 'Apache 2.0'
 description 'Provides mysql_service and mysql_client resources'
 
-version '5.1.0'
+version '5.1.1'
 
 supports 'amazon'
 supports 'redhat'
