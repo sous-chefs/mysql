63f292ad6d49bdd1dcb06a75e13a5aa658b4b07a
Stephan Renatus <s.renatus@x-ion.de>
2014-04-11 09:28:07 -0400

    [COOK-4547] - Shellescape server_root_password
    Signed-off-by: Sean OMeara <someara@opscode.com>

--- libraries/provider_mysql_service_fedora.rb
+++ libraries/provider_mysql_service_fedora.rb
@@ -1,4 +1,5 @@
 require 'chef/provider/lwrp_base'
+require 'shellwords'
 
 class Chef
   class Provider
@@ -73,7 +74,7 @@ class Chef
             if node['mysql']['server_root_password'].empty?
               pass_string = ''
             else
-              pass_string = "-p#{node['mysql']['server_root_password']}"
+              pass_string = '-p' + Shellwords.escape(node['mysql']['server_root_password'])
             end
 
             execute 'install-grants' do
@@ -123,7 +124,7 @@ class Chef
             execute 'assign-root-password' do
               cmd = "#{prefix_dir}/bin/mysqladmin"
               cmd << ' -u root password '
-              cmd << node['mysql']['server_root_password']
+              cmd << Shellwords.escape(node['mysql']['server_root_password'])
               command cmd
               action :run
               only_if "#{prefix_dir}/bin/mysql -u root -e 'show databases;'"
