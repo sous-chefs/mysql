9f15de848c651d17ab36aef97ad74d2decf51baf
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-06-02 17:13:57 -0700

    reduce duplication in provider_mysql_client implementations

--- libraries/provider_mysql_client.rb
+++ libraries/provider_mysql_client.rb
@@ -3,7 +3,34 @@ require 'chef/provider'
 class Chef
   class Provider
     class MysqlClient < Chef::Provider::LWRPBase
-      def action_create
+      use_inline_resources if defined?(use_inline_resources)
+
+      def whyrun_supported?
+        true
+      end
+
+      action :create do
+        converge_by 'installing packages' do
+          packages.each do |p|
+            package p do
+              action :install
+            end
+          end
+        end
+      end
+
+      action :delete do
+        converge_by 'removing packages' do
+          packages.each do |p|
+            package p do
+              action :remove
+            end
+          end
+        end
+      end
+
+      def packages
+        %w()
       end
     end
   end
