2da23a220d605caa145cb1810edfb85e4ffe9362
Sean OMeara <someara@opscode.com>
2014-06-18 17:24:38 -0400

    Version bump to 5.3.6

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@getchef.com'
 license 'Apache 2.0'
 description 'Provides mysql_service and mysql_client resources'
 
-version '5.3.4'
+version '5.3.6'
 
 supports 'amazon'
 supports 'redhat'
