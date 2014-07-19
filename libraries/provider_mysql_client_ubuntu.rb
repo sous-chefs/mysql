66c8733aaaa37e58ffa0a78c56e0a7a2f4520ccb
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2014-07-19 12:19:00 +0200

    fix missing version for mysql-client packages;

--- libraries/provider_mysql_client_ubuntu.rb
+++ libraries/provider_mysql_client_ubuntu.rb
@@ -5,27 +5,27 @@ class Chef
     class MysqlClient
       class Ubuntu < Chef::Provider::MysqlClient
         use_inline_resources if defined?(use_inline_resources)
-
         def whyrun_supported?
           true
         end
-
         action :create do
           converge_by 'ubuntu pattern' do
-            %w(mysql-client libmysqlclient-dev).each do |p|
-              package p do
-                action :install
-              end
+            package "mysql-client-#{node["mysql"]["version"]}" do
+              action :install
+            end
+            package "libmysqlclient-dev" do
+              action :install
             end
           end
         end
 
         action :delete do
           converge_by 'ubuntu pattern' do
-            %w(mysql-client libmysqlclient-dev).each do |p|
-              package p do
-                action :remove
-              end
+            package "mysql-client-#{node["mysql"]["version"]}" do
+              action :remove
+            end
+            package "libmysqlclient-dev" do
+              action :remove
             end
           end
         end
