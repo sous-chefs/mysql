e9d3ee20d30baed961210e0af0eb727419109f3c
Scott Lavender <scott.lavender@equifax.com>
2014-07-23 11:56:06 -0400

    Added libmysqlclient-devel package to SuSE client provider.

--- libraries/provider_mysql_client_suse.rb
+++ libraries/provider_mysql_client_suse.rb
@@ -12,7 +12,7 @@ class Chef
 
         action :create do
           converge_by 'suse pattern' do
-            %w(mysql-client).each do |p|
+            %w(mysql-client libmysqlclient-devel).each do |p|
               package p do
                 action :install
               end
@@ -22,7 +22,7 @@ class Chef
 
         action :delete do
           converge_by 'suse pattern' do
-            %w(mysql-client).each do |p|
+            %w(mysql-client libmysqlclient-devel).each do |p|
               package p do
                 action :remove
               end
