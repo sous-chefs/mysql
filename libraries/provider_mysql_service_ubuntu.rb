edd6a4625d766f50658e54a0ddba58ed54770355
Brian Dwyer <bdwyer@ieee.org>
2014-08-18 15:19:06 -0400

    Ubuntu - Added ability to change MySQL root password

--- libraries/provider_mysql_service_ubuntu.rb
+++ libraries/provider_mysql_service_ubuntu.rb
@@ -88,6 +88,8 @@ class Chef
               pass_string = '-p' + Shellwords.escape(new_resource.server_root_password)
             end
 
+            pass_string = '-p' + ::File.open('/etc/.mysql_root').read.chomp if ::File.exists?('/etc/.mysql_root')
+
             execute 'install-grants' do
               cmd = "#{prefix_dir}/bin/mysql"
               cmd << ' -u root '
@@ -187,6 +189,15 @@ class Chef
               creates "#{new_resource.data_dir}/ibdata1"
               creates "#{new_resource.data_dir}/ib_logfile0"
               creates "#{new_resource.data_dir}/ib_logfile1"
+            end
+
+            execute 'create root marker' do
+              cmd = '/bin/echo'
+              cmd << " '#{Shellwords.escape(new_resource.server_root_password)}'"
+              cmd << ' > /etc/.mysql_root'
+              cmd << ' ;/bin/chmod 0600 /etc/.mysql_root'
+              command cmd
+              action :run
             end
           end
         end
