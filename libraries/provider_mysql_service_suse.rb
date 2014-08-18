af9d8a3d71cd3277081b69060798d6acfb078045
Brian Dwyer <bdwyer@ieee.org>
2014-08-18 15:18:55 -0400

    SUSE - Added ability to change MySQL root password

--- libraries/provider_mysql_service_suse.rb
+++ libraries/provider_mysql_service_suse.rb
@@ -114,6 +114,8 @@ class Chef
               pass_string = '-p' + Shellwords.escape(new_resource.server_root_password)
             end
 
+            pass_string = '-p' + ::File.open('/etc/.mysql_root').read.chomp if ::File.exists?('/etc/.mysql_root')
+
             execute 'install-grants' do
               cmd = '/usr/bin/mysql'
               cmd << ' -u root '
@@ -141,6 +143,15 @@ class Chef
               command cmd
               action :run
               only_if "/usr/bin/mysql -u root -e 'show databases;'"
+            end
+
+            execute 'create root marker' do
+              cmd = '/bin/echo'
+              cmd << " '#{Shellwords.escape(new_resource.server_root_password)}'"
+              cmd << ' > /etc/.mysql_root'
+              cmd << ' ;/bin/chmod 0600 /etc/.mysql_root'
+              command cmd
+              action :run
             end
           end
         end
