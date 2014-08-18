2285b7afc3389986833a6b731019e99979eb600d
Brian Dwyer <bdwyer@ieee.org>
2014-08-18 15:18:28 -0400

    SmartOS - Added ability to change MySQL root password

--- libraries/provider_mysql_service_smartos.rb
+++ libraries/provider_mysql_service_smartos.rb
@@ -181,12 +181,23 @@ class Chef
               pass_string = '-p' + Shellwords.escape(new_resource.server_root_password)
             end
 
+            pass_string = '-p' + ::File.open('/etc/.mysql_root').read.chomp if ::File.exists?('/etc/.mysql_root')
+
             execute 'install-grants' do
               cmd = "#{prefix_dir}/bin/mysql"
               cmd << ' -u root '
               cmd << "#{pass_string} < #{prefix_dir}/etc/mysql_grants.sql"
               command cmd
               action :nothing
+            end
+
+            execute 'create root marker' do
+              cmd = '/bin/echo'
+              cmd << " '#{Shellwords.escape(new_resource.server_root_password)}'"
+              cmd << ' > #{prefix_dir}/etc/.mysql_root'
+              cmd << ' ;/bin/chmod 0600 #{prefix_dir}/etc/.mysql_root'
+              command cmd
+              action :run
             end
           end
         end
