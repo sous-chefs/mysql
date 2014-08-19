2a660e2c522818b2e46a53460522a808fda58cbf
Brian Dwyer <bdwyer@ieee.org>
2014-08-19 10:25:48 -0400

    Fix string interpolation

--- libraries/provider_mysql_service_smartos.rb
+++ libraries/provider_mysql_service_smartos.rb
@@ -181,7 +181,7 @@ class Chef
               pass_string = '-p' + Shellwords.escape(new_resource.server_root_password)
             end
 
-            pass_string = '-p' + ::File.open('/etc/.mysql_root').read.chomp if ::File.exist?('/etc/.mysql_root')
+            pass_string = '-p' + ::File.open("#{prefix_dir}/etc/.mysql_root").read.chomp if ::File.exist?("#{prefix_dir}/etc/.mysql_root")
 
             execute 'install-grants' do
               cmd = "#{prefix_dir}/bin/mysql"
@@ -194,8 +194,8 @@ class Chef
             execute 'create root marker' do
               cmd = '/bin/echo'
               cmd << " '#{Shellwords.escape(new_resource.server_root_password)}'"
-              cmd << ' > #{prefix_dir}/etc/.mysql_root'
-              cmd << ' ;/bin/chmod 0600 #{prefix_dir}/etc/.mysql_root'
+              cmd << " > #{prefix_dir}/etc/.mysql_root"
+              cmd << " ;/bin/chmod 0600 #{prefix_dir}/etc/.mysql_root"
               command cmd
               action :run
             end
