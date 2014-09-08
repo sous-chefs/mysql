1476860d76fa6e6a6c79f7bd2c5b6ad1b83590f6
Sean OMeara <someara@opscode.com>
2014-09-08 10:36:28 -0400

    Revert "fix debian pass_string"
    This reverts commit 53dcd233e8387f7a657802f963b77c32ed080ec1.

--- libraries/helpers_debian.rb
+++ libraries/helpers_debian.rb
@@ -25,10 +25,10 @@ module MysqlCookbook
         if new_resource.parsed_server_root_password.empty?
           pass_string = ''
         else
-          pass_string = '-p ' + Shellwords.escape(new_resource.parsed_server_root_password)
+          pass_string = '-p' + Shellwords.escape(new_resource.parsed_server_root_password)
         end
 
-        pass_string = '-p ' + ::File.open('/etc/.mysql_root').read.chomp if ::File.exist?('/etc/.mysql_root')
+        pass_string = '-p' + ::File.open('/etc/.mysql_root').read.chomp if ::File.exist?('/etc/.mysql_root')
         pass_string
       end
 
