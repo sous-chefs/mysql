bed9a00285ac75baa9e7a98c991b4d1a0c843932
Jeremy Shipman <jeremy@burnbright.net>
2014-09-24 16:16:55 +1200

    Introduced platform info for amazon version 2014.09

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -95,7 +95,18 @@ module Opscode
               },
               '5.6' => {
                 'package_name' => 'mysql-community-server'
-              }
+              },
+            },
+            '2014.09' => {
+              '5.1' => {
+                'package_name' => 'mysql51-server'
+              },
+              '5.5' => {
+                'package_name' => 'mysql-server'
+              },
+              '5.6' => {
+                'package_name' => 'mysql-server'
+              },
             }
           },
           'fedora' => {
