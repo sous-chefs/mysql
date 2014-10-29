09c536423a902667e4f896578e1c9291ce5a0ffb
Sean OMeara <someara@opscode.com>
2014-10-29 13:12:11 -0400

    2014.09 => {

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -103,6 +103,17 @@ module Opscode
               }
             }
           },
+          '2014.09' => {
+            '5.1' => {
+              'package_name' => 'mysql51-server'
+            },
+            '5.5' => {
+              'package_name' => 'mysql-server'
+            },
+            '5.6' => {
+              'package_name' => 'mysql-server'
+            },
+          },          
           'fedora' => {
             '19' => {
               '5.5' => {
