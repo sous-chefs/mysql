e33580d68d78344bd2a5983dd5c8a9413540905b
Sean OMeara <sean@chef.io>
2015-02-26 12:34:48 -0500

    issue 303: Fixing debian system service :disable action

--- Gemfile
+++ Gemfile
@@ -2,11 +2,12 @@ source 'https://rubygems.org'
 
 group :lint do
   gem 'foodcritic', '~> 4.0'
-  gem 'rubocop',    '= 0.27.1'
+  gem 'rubocop', '~> 0.18'
+  gem 'rainbow', '< 2.0'
+  gem 'rake'
 end
 
 group :unit do
-  gem 'berkshelf',  '~> 3.2'
   gem 'chefspec',   '~> 4.2'
 end
 
@@ -14,13 +15,13 @@ group :kitchen_common do
   gem 'test-kitchen', '~> 1.3'
 end
 
-group :kitchen_vagrant do
-  gem 'kitchen-vagrant'
-end
-
-group :kitchen_cloud do
-  gem 'kitchen-digital_ocean'
-  gem 'kitchen-ec2'
-  gem 'kitchen-joyent'
-  gem 'kitchen-gce'
+group :development do
+  gem 'ruby_gntp'
+  gem 'growl'
+  gem 'rb-fsevent'
+  gem 'guard', '~> 2.4'
+  gem 'guard-kitchen'
+  gem 'guard-foodcritic'
+  gem 'guard-rspec'
+  gem 'guard-rubocop'
 end
--- libraries/provider_mysql_service_sysvinit.rb
+++ libraries/provider_mysql_service_sysvinit.rb
@@ -26,7 +26,8 @@ class Chef
 
           service "#{new_resource.name} :start #{mysql_name}" do
             service_name mysql_name
-            provider Chef::Provider::Service::Init::Insserv if node['platform'] == 'debian'
+            provider Chef::Provider::Service::Init::Redhat if node['platform_family'] == 'redhat'
+            provider Chef::Provider::Service::Init::Insserv if node['platform_family'] == 'debian'
             supports restart: true, status: true
             action [:enable, :start]
           end
@@ -35,7 +36,8 @@ class Chef
         action :stop do
           service "#{new_resource.name} :stop #{mysql_name}" do
             service_name mysql_name
-            provider Chef::Provider::Service::Init
+            provider Chef::Provider::Service::Init::Redhat if node['platform_family'] == 'redhat'
+            provider Chef::Provider::Service::Init::Insserv if node['platform_family'] == 'debian'
             supports restart: true, status: true
             action [:stop]
           end
@@ -44,7 +46,8 @@ class Chef
         action :restart do
           service "#{new_resource.name} :restart #{mysql_name}" do
             service_name mysql_name
-            provider Chef::Provider::Service::Init
+            provider Chef::Provider::Service::Init::Redhat if node['platform_family'] == 'redhat'
+            provider Chef::Provider::Service::Init::Insserv if node['platform_family'] == 'debian'
             supports restart: true
             action :restart
           end
@@ -53,7 +56,8 @@ class Chef
         action :reload do
           service "#{new_resource.name} :reload #{mysql_name}" do
             service_name mysql_name
-            provider Chef::Provider::Service::Init
+            provider Chef::Provider::Service::Init::Redhat if node['platform_family'] == 'redhat'
+            provider Chef::Provider::Service::Init::Insserv if node['platform_family'] == 'debian'
             action :reload
           end
         end
@@ -61,7 +65,8 @@ class Chef
         def create_stop_system_service
           service "#{new_resource.name} :create #{system_service_name}" do
             service_name system_service_name
-            provider Chef::Provider::Service::Init
+            provider Chef::Provider::Service::Init::Redhat if node['platform_family'] == 'redhat'
+            provider Chef::Provider::Service::Init::Insserv if node['platform_family'] == 'debian'
             supports status: true
             action [:stop, :disable]
           end
@@ -70,7 +75,8 @@ class Chef
         def delete_stop_service
           service "#{new_resource.name} :delete #{mysql_name}" do
             service_name mysql_name
-            provider Chef::Provider::Service::Init
+            provider Chef::Provider::Service::Init::Redhat if node['platform_family'] == 'redhat'
+            provider Chef::Provider::Service::Init::Insserv if node['platform_family'] == 'debian'
             supports status: true
             action [:disable, :stop]
             only_if { ::File.exist?("#{etc_dir}/init.d/#{mysql_name}") }
