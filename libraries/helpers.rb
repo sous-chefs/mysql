50c633cdc06f6ec54a973281d3d4e276fa733ce5
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-05-07 11:11:44 -0400

    Update pkginfo for Debian Jessie (8). Fixes #334.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -264,8 +264,8 @@ EOSQL
         @pkginfo.set['debian']['7']['5.6']['server_package'] = 'mysql-server-5.6'
         @pkginfo.set['debian']['7']['5.7']['client_package'] = %w(mysql-client libmysqlclient-dev) # apt-repo from dotdeb
         @pkginfo.set['debian']['7']['5.7']['server_package'] = 'mysql-server-5.7'
-        @pkginfo.set['debian']['jessie/sid']['5.5']['client_package'] = %w(mysql-client libmysqlclient-dev)
-        @pkginfo.set['debian']['jessie/sid']['5.5']['server_package'] = 'mysql-server-5.5'
+        @pkginfo.set['debian']['8']['5.5']['client_package'] = %w(mysql-client libmysqlclient-dev)
+        @pkginfo.set['debian']['8']['5.5']['server_package'] = 'mysql-server-5.5'
         @pkginfo.set['fedora']['20']['5.5']['client_package'] = %w(community-mysql community-mysql-devel)
         @pkginfo.set['fedora']['20']['5.5']['server_package'] = 'community-mysql-server'
         @pkginfo.set['fedora']['20']['5.6']['client_package'] = %w(mysql-community-client mysql-community-devel)
@@ -401,8 +401,8 @@ EOSQL
       return '5.5' if node['platform_family'] == 'debian' && node['platform_version'] == '13.10'
       return '5.5' if node['platform_family'] == 'debian' && node['platform_version'] == '14.04'
       return '5.5' if node['platform_family'] == 'debian' && node['platform_version'] == '14.10'
-      return '5.5' if node['platform_family'] == 'debian' && node['platform_version'] == 'jessie/sid'
       return '5.5' if node['platform_family'] == 'debian' && node['platform_version'].to_i == 7
+      return '5.5' if node['platform_family'] == 'debian' && node['platform_version'].to_i == 8
       return '5.5' if node['platform_family'] == 'fedora'
       return '5.5' if node['platform_family'] == 'freebsd'
       return '5.5' if node['platform_family'] == 'omnios'
