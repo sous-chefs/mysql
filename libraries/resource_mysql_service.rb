d9ffdb90192d172f4312fc4382bc24be214450eb
geleman <gregoryeric.lane@gmail.com>
2015-06-25 10:40:33 -0400

    Update resource_mysql_service.rb

--- libraries/resource_mysql_service.rb
+++ libraries/resource_mysql_service.rb
@@ -21,6 +21,8 @@ class Chef
       attribute :socket, kind_of: String, default: nil
       attribute :version, kind_of: String, default: nil
       attribute :error_log, kind_of: String, default: nil
+      attribute :tmp_dir, kind_of: String, default: nil
+      attribute :pid_file, kind_of: String, default: nil
     end
   end
 end
