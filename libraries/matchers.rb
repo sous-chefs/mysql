1ae87d422f46e0af37d9dbeb794866ae2f3f564e
Xabier de Zuazo <xabier@zuazo.org>
2015-09-15 20:23:28 +0200

    Add missing mysql_service matchers: start, stop, restart, reload

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -17,6 +17,22 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:mysql_service, :delete, resource_name)
   end
 
+  def start_mysql_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:mysql_service, :start, resource_name)
+  end
+
+  def stop_mysql_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:mysql_service, :stop, resource_name)
+  end
+
+  def restart_mysql_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:mysql_service, :restart, resource_name)
+  end
+
+  def reload_mysql_service(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:mysql_service, :reload, resource_name)
+  end
+
   # client
   def create_mysql_client(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:mysql_client, :create, resource_name)
