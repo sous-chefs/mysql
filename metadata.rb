29b25afbe384c779d9b6de6c46afe97f44470809
Tim Smith <tsmith84@gmail.com>
2015-11-02 11:28:53 -0800

    Add additional metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Provides mysql_service, mysql_config, and mysql_client resources'
-
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '6.1.2'
 
 supports 'amazon'
@@ -19,3 +19,6 @@ supports 'suse'
 
 depends 'yum-mysql-community'
 depends 'smf'
+
+source_url 'https://github.com/chef-cookbooks/mysql' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/mysql/issues' if respond_to?(:issues_url)
