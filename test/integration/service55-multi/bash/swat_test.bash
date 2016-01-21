874a1fd95c1bbbfbdd92d1e2791a5b22a6143f5c
Alexey Melezhik <melezhik@gmail.com>
2016-01-21 13:36:19 +0300

    add new box

--- test/integration/service55-multi/bash/swat_test.bash
+++ test/integration/service55-multi/bash/swat_test.bash
@@ -5,7 +5,7 @@ sparrow check add foo mysql
 sparrow check set foo mysql outth-mysql-cookbook 127.0.0.1
 sparrow check load_ini foo mysql /tmp/outth-mysql-instance_1.ini
 sparrow check show foo mysql 
-sparrow check run foo mysql
+match_l=1000 sparrow check run foo mysql
 
 
 
