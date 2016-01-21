bc7e96d63c3ef8690027f000996d47795a3a540f
Alexey Melezhik <melezhik@gmail.com>
2016-01-21 14:28:52 +0300

    test both instances

--- test/fixtures/cookbooks/mysql_service_test/recipes/outth-ini.rb
+++ test/fixtures/cookbooks/mysql_service_test/recipes/outth-ini.rb
@@ -6,3 +6,13 @@ template '/tmp/outth-mysql-instance_1.ini' do
         :password => 'ilikerandompasswords'
     }) 
 end
+
+
+template '/tmp/outth-mysql-instance_2.ini' do
+    source 'outth-mysql.ini.erb'
+    variables({ 
+        :port => 3308,
+        :user => 'root',
+        :password => 'string with spaces'
+    }) 
+end
---
+++ test/integration/service55-multi/bash/instance2_test.bash
@@ -0,0 +1,12 @@
+PATH=$PATH:/usr/local/bin/
+sparrow 1>/dev/null || exit 1
+sparrow project create foo
+sparrow check add foo mysql
+sparrow check set foo mysql outth-mysql-cookbook 127.0.0.1
+sparrow check load_ini foo mysql /tmp/outth-mysql-instance_2.ini
+sparrow check show foo mysql 
+match_l=1000 sparrow check run foo mysql
+
+
+
+     
