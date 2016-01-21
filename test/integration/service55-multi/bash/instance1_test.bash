3e829400918cdb0cd5e47349aa4db01f1b38d68d
Alexey Melezhik <melezhik@gmail.com>
2016-01-21 17:13:46 +0300

    remove-useless-blank-lins

--- test/integration/service55-multi/bash/instance1_test.bash
+++ test/integration/service55-multi/bash/instance1_test.bash
@@ -6,7 +6,3 @@ sparrow check set foo mysql outth-mysql-cookbook 127.0.0.1
 sparrow check load_ini foo mysql /tmp/outth-mysql-instance_1.ini
 sparrow check show foo mysql 
 match_l=1000 sparrow check run foo mysql
-
-
-
-     
