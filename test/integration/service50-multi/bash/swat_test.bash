499311d8db28bed12be9430c44e18c583fcf6d04
Alexey Melezhik <melezhik@gmail.com>
2016-01-21 10:56:57 +0300

    fix plugin name

--- test/integration/service50-multi/bash/swat_test.bash
+++ test/integration/service50-multi/bash/swat_test.bash
@@ -2,7 +2,7 @@ PATH=$PATH:/usr/local/bin/
 sparrow 1>/dev/null || exit 1
 sparrow project create foo
 sparrow check add foo mysql
-sparrow check set foo mysql swat-mysql-cookbook 127.0.0.1
+sparrow check set foo mysql outth-mysql-cookbook 127.0.0.1
 sparrow check load_ini foo mysql /tmp/outth-mysql-instance_1.ini
 sparrow check show foo mysql 
 sparrow check run foo mysql
