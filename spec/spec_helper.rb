bc7974e791887ea9909e33e56b09b999a713627b
Tim Smith <tsmith@chef.io>
2016-09-26 09:57:42 -0700

    Use the doc formatter for specs
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,7 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 
 RSpec.configure do |config|
-  config.color = true
-  config.log_level = :error
+  config.color = true               # Use color in STDOUT
+  config.formatter = :documentation # Use the specified formatter
+  config.log_level = :error         # Avoid deprecation notice SPAM
 end
-
-at_exit { ChefSpec::Coverage.report! }
