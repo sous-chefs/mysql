26b124a0007a8cef71ab48539dab453e67bb74c2
vpllion <vikas.parashar@scalemonks.com>
2016-10-07 16:25:19 +0530

    Update helpers.rb
    change the mysql version from 5.6.31. to 5.6.33

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -168,7 +168,7 @@ module MysqlCookbook
 
       # amazon
       return '5.5.48-2.el6' if major_version == '5.5' && amazon?
-      return '5.6.31-2.el6' if major_version == '5.6' && amazon?
+      return '5.6.33-2.el6' if major_version == '5.6' && amazon?
       return '5.7.11-1.el6' if major_version == '5.7' && amazon?
 
       # N-1 fedora
