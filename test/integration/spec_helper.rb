7aae9b18653409c550e68492be807fe4b461dc9b
Aleksey Hariton <aleksey_hariton1@epam.com>
2017-01-08 00:18:12 +0300

    Make CookStyle happy

--- test/integration/spec_helper.rb
+++ test/integration/spec_helper.rb
@@ -2,15 +2,20 @@
 #
 # @param [String] version expected client version, for example '5.5'
 def check_client(version)
+  lib_name = 'libmysqlclient.so'
+  version_short = version.delete('.')
+
   case os[:family]
-    when 'centos'
-      mysql_cmd = "scl enable mysql#{version.delete('.')} \"mysql --version\"" if os[:release] =~ /^5\./
-    when 'smartos'
-      mysql_cmd = '/opt/local/bin/mysql --version'
-    when  'solaris'
-      mysql_cmd = '/opt/omni/bin/mysql --version'
-    else
-      mysql_cmd = '/usr/bin/mysql --version'
+  when 'centos'
+    mysql_cmd = "scl enable mysql#{version_short} \"mysql --version\"" if os[:release] =~ /^5\./
+  when 'smartos'
+    mysql_cmd = '/opt/local/bin/mysql --version'
+  when 'suse'
+    lib_name = "libmysql#{version_short}client.so"
+  when 'solaris'
+    mysql_cmd = '/opt/omni/bin/mysql --version'
+  else
+    mysql_cmd = '/usr/bin/mysql --version'
   end
 
   describe command(mysql_cmd) do
@@ -19,7 +24,7 @@ def check_client(version)
   end
 
   describe command('ldconfig -p') do
-    its(:stdout) { should match /libmysqlclient.so/}
+    its(:stdout) { should match(/#{lib_name}/) }
   end
 
   describe file('/usr/include/mysql/mysql.h') do
