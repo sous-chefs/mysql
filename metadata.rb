362a2abcd0ca85c4e278c29693323a6ef902ac0d
Tim Smith <tsmith@chef.io>
2017-04-04 11:26:33 -0700

    Switch to Delivery Local and rename the docked config
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'mysql'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Provides mysql_service, mysql_config, and mysql_client resources'
 version '8.3.0'
 
