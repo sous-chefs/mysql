9a66e575a16bfd448ed51f3970ad4eacc78e4413
Ali M <awm086@gmail.com>
2018-06-20 12:53:24 -0400

    Mysql ubuntu16 mysql 5.7 (#543)
    * Ensure compatibility with mysql 5.7 on Ubtuntu 16.04

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -81,7 +81,7 @@ module MysqlCookbook
 
     def mysql_name
       if instance == 'default'
-        'mysql'
+        "mysql"
       else
         "mysql-#{instance}"
       end
@@ -189,13 +189,16 @@ module MysqlCookbook
       # mysql will read \& as &, but \% as \%. Just escape bare-minimum \ and '
       sql_escaped_password = root_password.gsub('\\') { '\\\\' }.gsub("'") { '\\\'' }
 
+      cmd = "UPDATE mysql.user SET #{password_column_name}=PASSWORD('#{sql_escaped_password}')#{password_expired} WHERE user = 'root';"
+      cmd = "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '#{sql_escaped_password}';" if v57plus
+
       <<-EOS
         set -e
         rm -rf /tmp/#{mysql_name}
         mkdir /tmp/#{mysql_name}
 
         cat > /tmp/#{mysql_name}/my.sql <<-'EOSQL'
-UPDATE mysql.user SET #{password_column_name}=PASSWORD('#{sql_escaped_password}')#{password_expired} WHERE user = 'root';
+#{cmd}
 DELETE FROM mysql.user WHERE USER LIKE '';
 DELETE FROM mysql.user WHERE user = 'root' and host NOT IN ('127.0.0.1', 'localhost');
 FLUSH PRIVILEGES;
@@ -205,12 +208,19 @@ EOSQL
 
        #{db_init}
        #{record_init}
+       #{wait_for_init}
 
+       EOS
+    end
+
+    def wait_for_init 
+      cmd = <<-EOS
        while [ ! -f #{pid_file} ] ; do sleep 1 ; done
        kill `cat #{pid_file}`
        while [ -f #{pid_file} ] ; do sleep 1 ; done
        rm -rf /tmp/#{mysql_name}
        EOS
+       cmd = "" if v57plus
     end
 
     def password_column_name
@@ -264,6 +274,11 @@ EOSQL
       return '/usr/libexec/mysqld' if node['platform_family'] == 'fedora'
       return 'mysqld' if scl_package?
       "#{prefix_dir}/usr/sbin/mysqld"
+    end
+
+    def mysql_systemd
+      return "/usr/share/mysql/mysql-systemd-start" if v57plus
+      "/usr/libexec/#{mysql_name}-wait-ready $MAINPID"
     end
 
     def mysqld_initialize_cmd
