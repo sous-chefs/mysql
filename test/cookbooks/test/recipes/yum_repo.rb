538361c455769b8bbe1bb22a2ac1ce6f59c2f982
Brian Green <brian.green@cerner.com>
2019-02-19 14:44:09 -0600

    add repo for 8.0 tests

--- test/cookbooks/test/recipes/yum_repo.rb
+++ test/cookbooks/test/recipes/yum_repo.rb
@@ -6,5 +6,20 @@ unless node['mysql'].nil?
     include_recipe 'yum-mysql-community::mysql56'
   when '5.7'
     include_recipe 'yum-mysql-community::mysql57'
+  when '8.0'
+    if node['platform_family'] == 'rhel'
+      distro = 'el'
+    elsif node['platform_family'] == 'fedora'
+      distro = 'fc'
+    end
+    yum_repository 'mysql80-community' do
+      description 'MySQL 8.0 Community Server'
+      baseurl "http://repo.mysql.com/yum/mysql-8.0-community/#{distro}/$releasever/$basearch"
+      enabled true
+      failovermethod 'priority'
+      fastestmirror_enabled false
+      gpgcheck true
+      gpgkey 'http://repo.mysql.com/RPM-GPG-KEY-mysql'
+    end
   end
 end
