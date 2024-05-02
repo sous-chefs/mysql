7e36523ed61609a0e4371d926632357d85d52c4b
Dan Webb <dan.webb@damacus.io>
2024-05-02 14:29:32 +0100

    Remove yum repo temporarily
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/cookbooks/test/recipes/yum_repo.rb
+++ test/cookbooks/test/recipes/yum_repo.rb
@@ -1,7 +1,7 @@
 # Before that, we use "native" versions
 
-unless node['mysql_test'].nil? || platform_family?('debian')
-  yum_mysql_community_repo 'mysql_repo' do
-    version node['mysql_test']['version']
-  end
-end
+# unless node['mysql_test'].nil? || platform_family?('debian')
+#   yum_mysql_community_repo 'mysql_repo' do
+#     version node['mysql_test']['version']
+#   end
+# end
